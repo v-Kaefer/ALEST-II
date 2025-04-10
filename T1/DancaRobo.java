a95411f155fd5c6dc010d6c9b3066f4adfa7c9be
Vitor Guttler <128633407+v-Kaefer@users.noreply.github.com>
2025-04-10 15:18:30 -0300

    Update DancaRobo.java

--- T1/DancaRobo.java
+++ T1/DancaRobo.java
@@ -1,22 +1,46 @@
 package T1;
-import java.util.HashSet; // Import the HashSet class
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 
 public class DancaRobo {
     public static void main(String[] args) {
-        // Primeira Receita ex:
-        HashSet<String> filaDeRobos = new HashSet<String>();
-        
 
-        int[] receita01 = {0, 1, 2, 3, 4, 5, 6};
+        // Receeeeita que os robôs começam (0 a n-1)
+        int n = 7; // Total de Robôs
+        int[] receitaInicial = new int[n];
+        // Primeira Receita ex:
+        for (int i = 0; i < n; i++) {
+            receitaInicial[i] = i;
+        }
+
+
+        HashSet<String> filaDeRobos = new HashSet<String>();
+        HashMap<HashSet, Integer> receitasUsadas = new HashMap<HashSet, Integer>();
+        
+        int rodada = 0;
+        String receitaAtual;
+
+        while (auxContagemReceita < filaDeRobos.size()) {
+            auxContagemReceita = auxContagemReceita++;
+
+
+
+            System.out.println(filaDeRobos+"operações: "+auxContagemReceita);
+        }
+
+        int[] receitaInicial = {0, 1, 2, 3, 4, 5, 6};
         int[] receita02 = {0, 1, 2, 3, 4, 5, 7};
 
-        filaDeRobos.add(java.util.Arrays.toString(receita01));
-        filaDeRobos.add(java.util.Arrays.toString(receita01));
+        filaDeRobos.add(java.util.Arrays.toString(receitaInicial));
+        filaDeRobos.add(java.util.Arrays.toString(receitaInicial));
         filaDeRobos.add(java.util.Arrays.toString(receita02));
 
         System.out.println(java.util.Arrays.toString(receita01)+" em hash:\n"+filaDeRobos);
+        System.out.println(filaDeRobos+"operações: "+auxContagemReceita);
         
     }
+
 
 }
